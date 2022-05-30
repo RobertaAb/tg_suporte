a373a13f00347687fce9ab154d2f3242dc8440f3
Roberta Abreu <robertaabreu77@gmail.com>
2022-05-30 08:07:37 -0300

    reorganizando estrutura repositorio

--- src/main/java/gov/fatec/tg_suporte/repository/ProcedimentoRepository.java
+++
@@ -1,9 +0,0 @@
-package gov.fatec.tg_suporte.repository;
-
-import gov.fatec.tg_suporte.model.ProcedimentoModel;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.UUID;
-
-public interface ProcedimentoRepository extends JpaRepository<ProcedimentoModel, UUID> {
-}
